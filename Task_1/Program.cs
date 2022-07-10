5403a7a8d4a40f27ddf90e953c5bf058e9ee4b17
Владимир Ворона <vvorona665@gmail.com>
2022-07-10 22:50:44 +0300

    Copleted task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N
+
+Console.WriteLine("Введите число M ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumNamber(int M, int N)
+{
+    int sum = 0;
+    if (M <= N)
+    {
+       sum = sum + M;
+       sum+= SumNamber(M+1, N);
+        
+    }
+    return sum;
+}
+
+
+Console.WriteLine($"Сумма чисел в промежутке = {SumNamber(M,N)}");
