a32a86624d6801d7826a5e821f79ec0b7e7e19ed
Владимир Ворона <vvorona665@gmail.com>
2022-07-10 22:51:10 +0300

    Completed Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два отрицательных числа m и n.
+
+int Akkerman (int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else 
+    {
+        return Akkerman(m-1, Akkerman(m, n -1));
+    }
+}
+
+Console.WriteLine(Akkerman(2,4));
